8248a47a6e0a992894c079e015cdd97ba643aa0d
w-zengtao <so.zengtao@gmail.com>
2019-08-16 15:06:56 +0800

    [ADD] 完成比对逻辑

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ Dir[File.dirname(__FILE__) + "/models/*.rb"].map { |file| require file }
 Dir[File.dirname(__FILE__) + "/models/matches/*.rb"].map { |file| require file }
 Dir[File.dirname(__FILE__) + "/middlewares/*.rb"].map { |file| require file }
 
-hltv = Hltv.new
-WorkPool.instance
-ThreadsWait.all_waits(*WorkPool.instance.threads)
+# hltv = Hltv.new
+# WorkPool.instance
+# ThreadsWait.all_waits(*WorkPool.instance.threads)
+
+SourceTable.run
--- models/source_table.rb
+++ models/source_table.rb
@@ -5,4 +5,28 @@ class SourceTable < ActiveRecord::Base
   @db ||= YAML.load_file(File.join("config", "database.yml"))["source"]
   ActiveRecord::Base.establish_connection @db
   self.table_name = "t_src_cqi_region_month"
+  
+  
+  def self.run
+
+    SourceTable.find_each do |source|
+
+      puts "当前正在处理的 Source ID 是： #{source.id}"
+
+      target = source.load_target
+
+      if target.attributes.reject { |k, v| "id" == k } != source.attributes.reject { |k, v| "id" == k }
+        puts source.id
+      end
+    end
+  end
+  
+  def load_target
+    target = TargetTable.find_by(
+      code:     self.code,
+      datatime: self.datatime
+    )
+
+    return target
+  end
 end
